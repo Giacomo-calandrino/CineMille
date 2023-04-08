d9e7196485b0ef81b8629c6a040ad05c22740d4c
Giacomo-calandrino <giacomocalandrino@gmail.com>
2023-04-08 18:40:39 +0200

    homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'details', component: MovieDetailsComponent}
 ];
 
 @NgModule({
